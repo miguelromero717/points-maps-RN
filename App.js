e32e625f7579791f17d04084769d7e161ed8baba
Miguel Angel Romero <miguelromero717@gmail.com>
2021-08-15 17:41:44 -0500

    Add initial setup for type Modal

--- App.js
+++ App.js
@@ -1,27 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Dimensions, Modal, StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
-const window = Dimensions.get('screen')
+const window = Dimensions.get("screen");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}/>
+      <MapView style={styles.map} />
+      <Modal animationType="slide" transparent={true} visible={true}>
+        <View style={styles.center}>
+          <View style={styles.modalView}>
+            <Text>Hola Mundo</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    backgroundColor: "#fff",
+    borderRadius: 4,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
   map: {
     height: window.height,
-    width: window.width
-  },  
+    width: window.width,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
