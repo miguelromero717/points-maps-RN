f4fc6b8a705f5ed4b1a0e2ccbc7159654c5652d8
Miguel Angel Romero <miguelromero717@gmail.com>
2021-08-15 17:35:45 -0500

    Add MapView

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
+
+const window = Dimensions.get('screen')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    height: window.height,
+    width: window.width
+  },  
   container: {
     flex: 1,
     backgroundColor: '#fff',
